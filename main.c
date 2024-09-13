fb1a6ba748a6be118d0697388fbdeb862bc8724c
Allyson Pina <allyson.viana@edu.ufes.br>
2024-09-13 00:23:29 -0300

    first commit

--- main.c
+++ main.c
@@ -2,17 +2,19 @@
 
 int main()
 {
-    Labirinto labirinto;
+    Labirinto labirinto; // Estrutura para armazenar o labirinto
+
     int altura, largura;
 
-    scanf("%d %d", &altura, &largura);
+    scanf("%d %d", &altura, &largura); // Leitura das dimensões do labirinto
 
-    carregarLabirinto(&labirinto, altura, largura);
-    // printf("Labirinto gerado:\n");
-    imprimeLabirinto(&labirinto);
+    carregarLabirinto(&labirinto, altura, largura); // Carrega o labirinto com as dimensões especificadas
+
+    imprimeLabirinto(&labirinto); // Imprime a configuração inicial do labirinto
 
     printf("Buscando o menor caminho...\n");
-    resolverLabirinto(&labirinto);
+
+    resolverLabirinto(&labirinto); // Resolve o labirinto e imprime o resultado
 
     return 0;
 }
